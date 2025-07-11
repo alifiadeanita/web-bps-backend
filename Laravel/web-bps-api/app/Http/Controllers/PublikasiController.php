471b76f657a4951547238dfc79524a2f82ca5928
Fia <alifiadea922@gmail.com>
2025-07-11 18:48:40 +0700

    first commit

---
+++ Laravel/web-bps-api/app/Http/Controllers/PublikasiController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers; 
+ 
+use App\Models\Publikasi; 
+use Illuminate\Http\Request; 
+ 
+class PublikasiController extends Controller 
+{ 
+    public function index() 
+    { 
+        return Publikasi::all(); 
+    } 
+ 
+    public function store(Request $request) 
+    { 
+        $validated = $request->validate([ 
+            'title' => 'required|string|max:255', 
+            'releaseDate' => 'required|date', 
+            'description' => 'nullable|string', 
+            'coverUrl' => 'nullable|url', 
+        ]); 
+ 
+        $publikasi = Publikasi::create($validated); 
+        return response()->json($publikasi, 201); 
+    } 
+
+    public function show($id)
+    {
+        $publikasi = Publikasi::find($id);
+
+        if (!$publikasi) {
+            return response()->json(['message' => 'Publication not found'], 404);
+        }
+
+        return response()->json($publikasi);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $publikasi = Publikasi::find($id);
+
+        if (!$publikasi) {
+            return response()->json(['message' => 'Publication not found'], 404);
+        }
+
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'releaseDate' => 'required|date',
+            'description' => 'nullable|string',
+            'coverUrl' => 'nullable|url',
+        ]);
+
+        $publikasi->update($validated);
+
+        return response()->json(['message' => 'Publication updated successfully', 'publication' => $publikasi]);
+    }
+
+    public function destroy($id)
+    {
+        $publikasi = Publikasi::find($id);
+
+        if (!$publikasi) {
+            return response()->json(['message' => 'Publication not found'], 404);
+        }
+
+        $publikasi->delete();
+
+        return response()->json(['message' => 'Publication deleted successfully']);
+    }
+} 
